f31cb979d1cc1e0cedd956cdda6e5690103bf57e
Suman <rajaksuman670@gmail.com>
2022-08-19 12:50:36 +0530

    s

--- JAVASCRIPT/ctrlstm_loop.js
+++ JAVASCRIPT/ctrlstm_loop.js
@@ -14,4 +14,58 @@
 //CONDITIONAL (TERNARY) OPERATOR//
 //It is the Only Javascript Operator that takes three operands.
 
+//variablename = (condition) ? correct value : false value
 
+var age = 19;
+console.log(age = (age>=18) ? "You are Eligible to Vote" : "You are Not Eligible to Vote");
+
+
+
+//WHILE LOOP//
+//It first checks the condition then enters into the 'Block Scope'.
+
+console.log("\nUsing While Loop");
+var number=20;
+ while(number<=10)
+ {
+    //Block Scope
+    console.log(number);
+    number++;
+ }
+
+
+ //DO WHILE LOOP//
+ //First Value directly enters into the block scope then it checks the condition.
+
+ var number1=20;
+ console.log("\nUsing Do While Loop");
+ do{
+    //Block Scope
+    console.log(number1);
+    number1++;
+ } while(number1<=10);
+
+
+ //Use Debugger//
+ 
+//var number1 = 20; 
+//  do{
+//     debugger;
+//     console.log(number1);
+//     number1++;
+//  } while(number1<=10);
+
+//Now Open Google Chrome --> Inspect Element --> Console --> Paste the Code including debuggger --> Click 'Step to next step' and hover on the variable to see the value changing in each step of the variables...
+
+
+
+//FOR LOOP//
+//Print Table of 8
+
+for(var i=1;i<=10;i++)
+{
+    var tableof=8;
+    console.log(tableof + " * " + i + " = " + tableof*i);
+}
+
+//FOR IN LOOP//
---
+++ JAVASCRIPT/functions.js
@@ -0,0 +1,79 @@
+//FUNCTION DEFINITION//
+
+//Before we use a function, we need to define it.
+//Consist of function keyword , followed by: The name of function , A list of parameters to the function, enclosed in parentheses and seperated by commas.
+//The Javascript statements that define theh function, enclosed in curly brackets, {...}
+
+function sum() {
+    //Statement
+    var a = 10;
+    var b = 20;
+    var summing = a + b;
+    console.log(summing);
+}
+
+
+//FUNCTION CALLING//
+
+//Defining a function does not execute it.
+//A Javascript function is executed when "something" invokes it (calls it).
+
+sum();
+
+
+//DIFFERENCE BETWEEN FUNCTION ARGUMENTS AND FUNCTION PARAMETERS//
+
+//Function Parameters are the names listed in the function's definition
+//Function arguments are the real values passed to the function.
+
+function sum1(a, b) //Function Parameters
+{
+    var total = a + b;
+    console.log(total);
+}
+
+sum1(200, 300) //Function Arguments
+
+
+//WHY FUNCTIONS//
+
+//You can reuse code: Define the code once, and use it many times.
+//You can use the same code many times with different arguments.
+//to produce different results.
+
+//OR
+
+//A function is a group of reusable code which can be called anywhere in your program. This eliminates the need of writing the same code again and again.
+
+
+
+//FUNCTION EXPRESSIONS//
+//Function Expression simply means create a fucntion and put it into the variable 
+
+
+function sum2(x, y) {
+    return x + y;
+}
+
+var tsum = sum2(20, 30); //Styoring the Function in a variable
+console.log(tsum);
+
+
+
+//ANONYMOUS FUNCTION//
+//A function expression is similar to and has the same syntax as a function declaration one can define "named" function expressions (where the name of the expression might be used in the call stack for example) or "anaonymous" function expressions.
+
+var funcExp = function (p, q) {
+    return p + q;
+}
+
+console.log(funcExp(15, 15));
+
+//OR//
+
+var funExp = function (m, n) {
+    return m + n;
+}
+
+var sum3 = funExp(35, 45);
+console.log("Total Sum = "+sum3);
